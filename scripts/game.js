4cc59040bb358da33347885f2dcc3673386dffd4
Andrew Rasmussen <andrew.rasmussen1@aggiemail.usu.edu>
2022-02-09 23:04:34 -0700

    add commands

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,28 @@
+let lastTimeStamp = performance.now();
+let cancelNextRequest = true;
+
+const up = () => {
+  console.log('up')
+}
+const down = () => {
+  console.log('down')
+}
+const left = () => {
+  console.log('left')
+}
+const right = () => {
+  console.log('right')
+}
+const help = () => {
+  console.log('help')
+}
+const breadcrumbs = () => {
+  console.log('breadcrumbs')
+}
+const path = () => {
+  console.log('path')
+}
+
 function update(elapsedTime) {}
 
 function render() {}
@@ -22,6 +47,30 @@ function run() {
   cancelNextRequest = false;
   requestAnimationFrame(gameLoop);
 }
+
+const commands = {
+  'ArrowUp': up,
+  'ArrowLeft': left,
+  'ArrowRight': right,
+  'ArrowDown': down,
+  'w': up,
+  'd': right,
+  'a': left,
+  's': down,
+  'i': up,
+  'l': right,
+  'j': left,
+  'k': down,
+  'h': help,
+  'b': breadcrumbs,
+  'p': path
+}
+
+window.addEventListener('keydown', (e) => {
+  if(e.key in commands) {
+    commands[e.key]()
+  }
+})
 
 let maze = generate(10, 10)
 renderMaze(maze)
